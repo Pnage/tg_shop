23443f92c74d4caae963621e9838585290cb2449
Pnage <99756027+Pnage@users.noreply.github.com>
2022-04-29 14:00:08 +0300

    #2

--- bot.py
+++ bot.py
@@ -34,3 +34,30 @@ sql.execute("""CREATE TABLE IF NOT EXISTS admins (id TEXT, username TEXT)""")
 
 if sql.execute(f"SELECT * FROM products WHERE id = '9999999999'").fetchone() is None:
     sql.execute("INSERT INTO products VALUES (?,?,?,?,?,?)",(9999999999, None, None, None, None, None))
+
+#Сохранение всех изменений в БД
+db.commit()
+
+#Настройка бота
+bot = Bot(token=settings[1])
+dp = Dispatcher(bot)
+
+#Первое обновление категорий товаров
+sql_query = "SELECT category FROM products"
+sql.execute(sql_query)
+content = sql.fetchall()
+categorys = []
+for i in content:
+    if str(i[0]) == 'None':
+        continue
+    category = i[0].lower()
+    if not str(category) in categorys:
+        categorys.append(f'{category}')
+
+#Проверка пользователя на права администратора
+def admin_check(user_id):
+    sql.execute(f"SELECT id FROM admins WHERE id = '{user_id}'")
+    if sql.fetchone() is None:
+        return False
+    else:
+        return True
